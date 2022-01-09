465f451faa27386416bff4f5b764090d33981ed7
Aarash Feizi <aarash95@gmail.com>
2022-01-09 13:28:52 -0500

    fix minor error

--- datasets/hotels_dataset.py
+++ datasets/hotels_dataset.py
@@ -12,25 +12,3 @@ class HotelsDataset(BaseDataset):
         self.rename_labels()
 
         self.labels = list(self.data_dict.keys())
-
-    def __make_data_dict(self):
-        data_dict = {}
-
-        if self.data_file_path.endswith('.txt'):
-            self.__read_txt()
-        elif self.data_file_path.endswith('.csv'):
-            self.__read_csv()
-        else:
-            raise Exception(f'{self.data_file_path}: File type not supported')
-
-        # make sure label_list are all float
-        float_label_list = [float(l) for l in self.label_list]
-        self.label_list = float_label_list
-
-        for l in self.label_list:
-            data_dict[l] = []
-
-        for i, label in enumerate(self.label_list):
-            data_dict[label].append(i)
-
-        return data_dict
