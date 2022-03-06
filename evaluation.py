88b74a860ec715f287bca4d02061efa1f7e6df6b
Aarash Feizi <aarash95@gmail.com>
2022-03-05 22:49:44 -0500

    load multigpu htv2

--- evaluation.py
+++ evaluation.py
@@ -21,6 +21,8 @@ import model as htv2
 # python evaluation.py -chk ../SupContrast/save/SupCon/hotels_models/SupCon_hotels_resnet50_lr_0.01_decay_0.0001_bsz_32_temp_0.1_trial_0_cosine/last.pth -name SupCon_hotels_resnet50_lr_0.01_decay_0.0001_bsz_32_temp_0.1_trial_0_cosine/ --kset 1 2 4 8 10 100 1000 --model_type resnet50 -d hotels -dr ../../datasets/ --baseline supcontrastive --gpu_ids 6
 import utils
 
+# todo: visualize embeddings after training with each model to see how they look softtriple, proxy, htv2
+
 dataset_choices = ['cars', 'cub', 'hotels', 'hotels_small', 'hotelid-val', 'hotelid-test']
 BASELINE_MODELS = ['ours',
                    'softtriple',
@@ -187,7 +189,11 @@ def htv2_load_model_resnet50(save_path, args):
 
     if args.get('cuda'):
         net = net.cuda()
-    net = net.encoder
+
+    if args.get('trained_with_mltp_gpu'):
+        net = net.module.encoder
+    else:
+        net = net.encoder
 
     return net
 
