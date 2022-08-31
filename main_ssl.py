d3383cf57b35a2b887118f1198989c2594b8a52f
Aarash Feizi <aarash95@gmail.com>
2022-08-31 15:11:18 -0400

    add print statement

--- main_ssl.py
+++ main_ssl.py
@@ -88,7 +88,7 @@ def main():
     ssl_kwargs = {}
     if all_args.get('ssl'):
         ssl_transforms, ssl_transforms_names = utils.TransformLoader(all_args, scale=[0.8, 1.0], rotate=90).get_composed_transform(mode='train-ssl')
-        print(ssl_transforms_names)
+        print('Train-SSL transforms: ', ssl_transforms_names)
 
         ssl_kwargs = {'random_crop_resize_transform': ssl_transforms[0],
             'mask_in_transform':ssl_transforms[1],
