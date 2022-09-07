29505239c4aad40886cf803f2e85dc8514e0ed5c
Aarash Feizi <aarash95@gmail.com>
2022-09-07 15:17:58 -0400

    fix bug

--- utils.py
+++ utils.py
@@ -360,7 +360,7 @@ def get_data(args, mode, file_name='', transform=None, sampler_mode='kbatch',
         dataset.set_lbl2idx(lbl2idx, onehotencoder)
 
 
-    if SAMPLERS[sampler_mode] is not None or args.get('dataset') != 'imagenet':
+    if SAMPLERS[sampler_mode] is not None and args.get('dataset') in datasets.PREDEFINED_DATASETS:
         sampler = SAMPLERS[sampler_mode](dataset=dataset,
                                         batch_size=args.get('batch_size'),
                                         num_instances=args.get('num_inst_per_class'),
